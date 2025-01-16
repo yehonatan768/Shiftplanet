2c41924dc9797c24e5bd7aa736eabff0df4989c0
orgoren2 <orgoren3146@gmail.com>
2025-01-16 21:11:58 +0200

    1

--- app/src/main/java/com/example/shiftplanet/EmployeeHomePage.java
+++ app/src/main/java/com/example/shiftplanet/EmployeeHomePage.java
@@ -1,54 +1,76 @@
 package com.example.shiftplanet;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class EmployeeHomePage extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // אם התפריט נבחר, נרצה שה-ActionBarDrawerToggle יטפל בו
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this); // אפשרות לתמיכה בתצוגה בקצוות
         setContentView(R.layout.employee_home_page);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        // אתחול רכיבי ה-UI
+        drawerLayout = findViewById(R.id.employee_home_page);
+        navigationView = findViewById(R.id.nav_view);
+
+        // אתחול ה-ActionBarDrawerToggle
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        // הצגת כפתור ההhamburger
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // הוספת Listner לתפריט הניווט
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.e_my_profile) {
+                    Toast.makeText(EmployeeHomePage.this, "My profile clicked", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.work_arrangement) {
+                    Toast.makeText(EmployeeHomePage.this, "Work arrangement clicked", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.constraints) {
+                    Toast.makeText(EmployeeHomePage.this, "Constraints clicked", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.day_off) {
+                    Toast.makeText(EmployeeHomePage.this, "Day off clicked", Toast.LENGTH_SHORT).show();
+                }
+                return true; // מחזיר true כי הטיפול ב-item הושלם
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.employee_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.constraints) {
-            Toast.makeText(this, "Constraints clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.day_off) {
-            Toast.makeText(this, "Days off clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.notification) {
-            Toast.makeText(this, "Notification clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.shift_change) {
-            Toast.makeText(this, "Shift change clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.e_work_arrangement) {
-            Toast.makeText(this, "Work arrangement clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.requests_status) {
-            Toast.makeText(this, "requests_status clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.e_my_profile) {
-            Toast.makeText(this, "my profile clicked", Toast.LENGTH_SHORT).show();
-            return true;
+    public void onBackPressed() {
+        // אם הDrawer פתוח, נסגור אותו
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
-            return super.onOptionsItemSelected(item);
+            // אם הDrawer סגור, נקרא לפונקציה הבסיסית של backPressed
+            super.onBackPressed();
         }
     }
 }
