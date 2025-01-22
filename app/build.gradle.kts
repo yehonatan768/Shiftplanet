43ecbd9407a5e848e5129f28f123df52eb6acc4d
Yehonatan <yehonatan768@gmail.com>
2025-01-22 19:11:37 +0200

    unitTestForUI - ManagerDialogRequestDetailsTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,12 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.storage)
+    implementation(libs.androidx.espresso.intents)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.runner)
+    androidTestImplementation(libs.androidx.espresso.core.v351)
+    androidTestImplementation(libs.androidx.junit.v115)
 }
