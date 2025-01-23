631f978f4983ddda1716c08956ab5b5d75ef06e9
Yehonatan <yehonatan768@gmail.com>
2025-01-23 03:42:23 +0200

    unitTestForUI - ManagerDialogRequestDetailsTest

--- app/src/androidTest/java/com/example/shiftplanet/ManagerDialogRequestDetailsTest.java
+++ app/src/androidTest/java/com/example/shiftplanet/ManagerDialogRequestDetailsTest.java
@@ -14,6 +14,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.Assert.*;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
